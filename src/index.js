990ad887d469f35a0815b086e7396916635f935a
stacysorey <stacysorey@gmail.com>
2021-06-08 02:42:00 -0400

    add components folder and files to src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-fetch("http://localhost:3000/api/v1/journals")
+document.addEventListener("DOMContentLoaded", () => {
+  fetchJournals();
+})
+
+
+function fetchJournals(){
+  const journalsContainer = document.getElementById("journals-container")
+
+  fetch("http://localhost:3000/api/v1/journals")
   .then(r => r.json())
-  .then(data => console.log(data))
+  .then(data => {
+      data.forEach(function(journal){
+          journalsContainer.innerHTML += `<li>${journal.title}</li>`
+      })
+  })
   .catch(err => console.warn(err))
+}
