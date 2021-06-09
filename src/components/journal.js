73bd13bccb6a63e5fde7df0fe5e014be15c8eb4f
stacysorey <stacysorey@gmail.com>
2021-06-09 17:52:58 -0400

    create journal class constructor

--- src/components/journal.js
+++ src/components/journal.js
@@ -1,5 +1,13 @@
 //creates journal class
 
 class Journal {
-  
+  static all = []
+
+  constructor({id, title, entries}){
+    this.id = id
+    this.title = title
+    this.entries = entries.map(entry => new Entry(entry))
+
+    Journal.all.push(this)
+  }
 }
