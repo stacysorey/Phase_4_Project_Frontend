4931a395c18d5017b816c6e95b625193746b381b
stacysorey <stacysorey@gmail.com>
2021-06-09 16:33:41 -0400

    move fetch requests to cozyJournalAdapter.js

--- src/components/cozyJournalAdapter.js
+++ src/components/cozyJournalAdapter.js
@@ -1,1 +1,15 @@
 //handles all fetch requests
+
+// function fetchJournals(){
+//   const journalsContainer = document.getElementById("journals-container")
+
+//   fetch("http://localhost:3000/api/v1/journals")
+//   .then(r => r.json())
+//   .then(data => {
+//       data.forEach(function(journal){
+//           journalsContainer.innerHTML += `<li>${journal.title}</li>`
+//       })
+//   })
+//   .catch(err => console.warn(err))
+// }
+
--- src/index.js
+++ src/index.js
@@ -3,15 +3,3 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-function fetchJournals(){
-  const journalsContainer = document.getElementById("journals-container")
-
-  fetch("http://localhost:3000/api/v1/journals")
-  .then(r => r.json())
-  .then(data => {
-      data.forEach(function(journal){
-          journalsContainer.innerHTML += `<li>${journal.title}</li>`
-      })
-  })
-  .catch(err => console.warn(err))
-}
