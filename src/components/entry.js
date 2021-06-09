272ee84a5d57104bbeff1678ea8c2a6e5126ec41
stacysorey <stacysorey@gmail.com>
2021-06-09 18:05:14 -0400

    create entry class constructor

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,5 +1,14 @@
 //creates entry class
 class Entry {
   static all = []
-  
+
+  constructor({id, title, description, date, journal_id}){
+    this.id = id
+    this.title = title
+    this.description = description
+    this.date = date
+    this.journal_id = journal_id
+
+    Entry.all.push(this)
+  }
 }
