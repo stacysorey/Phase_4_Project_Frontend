d8b795c80be9a3d308f71b75726ee01599ac35fa
stacysorey <stacysorey@gmail.com>
2021-06-09 21:02:29 -0400

    create switch actions for event handling

--- src/components/journalForm.js
+++ src/components/journalForm.js
@@ -11,4 +11,25 @@ class JournalForm {
     journalsContainer.addEventListener("click", this.handleEvents);
   }
 
+  handleEvents(event){
+    const li = event.target.parentElement;
+    const action = event.target.dataset.action;
+    switch (action) {
+      case "delete":
+        console.log("delete event")
+        break;
+
+      case "edit":
+        console.log("edit event")
+        break;
+
+      case "show":
+        console.log("show event")
+        break;
+
+      default:
+        break;
+    }
+  }
+
 }
