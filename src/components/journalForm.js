c819271f716f7b4884548dabcfd9ffbb593aced4
stacysorey <stacysorey@gmail.com>
2021-06-09 20:52:39 -0400

    create JournalForm constructor

--- src/components/journalForm.js
+++ src/components/journalForm.js
@@ -1,8 +1,11 @@
 //events and forms
 class JournalForm {
 
+  constructor(){
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEvents = this.handleEvents.bind(this);
+  }
 
 
 
-  
 }
