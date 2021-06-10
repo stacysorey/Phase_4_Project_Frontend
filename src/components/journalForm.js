eec0a782299641eb39ee6a3c7ae0600df4805ea7
stacysorey <stacysorey@gmail.com>
2021-06-09 20:54:54 -0400

    create listen function

--- src/components/journalForm.js
+++ src/components/journalForm.js
@@ -2,10 +2,13 @@
 class JournalForm {
 
   constructor(){
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
     this.handleEvents = this.handleEvents.bind(this);
   }
 
-
+  listenEvents(){
+    const journalsContainer = document.getElementById("journals-container");
+    journalsContainer.addEventListener("click", this.handleEvents);
+  }
 
 }
