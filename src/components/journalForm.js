c5d4217b9ae5e922b4da28490f2812fafc10e10e
stacysorey <stacysorey@gmail.com>
2021-06-15 15:13:43 -0400

    finish  addCreateForm

--- src/components/journalForm.js
+++ src/components/journalForm.js
@@ -8,6 +8,11 @@ class JournalForm {
 
   addCreateForm(){
     const formContainer = document.getElementById("form-container");
+    const form = document.createElement('form');
+    form.innerHTML = `<input id="title-input" placeholder='title' type='text'/>
+    <input id="title-submit" value='Create Journal' type='submit'/>`
+    formContainer.append(form);
+    form.addEventListener("submit", this.handleSubmit);
   }
 
   listenEvents(){
